9052d31dba9e6e66c279a49b82ee12b3fb3743e5
Simeon <simeon.georev.97@hotmail.com>
2023-11-05 07:17:16 +0200

    Homework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Main{
 
     public static void main (String[] args){
-        System.out.println("            Hello");
+        System.out.println("         Hello ! ");
         System.out.println("What's your name?");
 
-        Scanner scanner = new scanner (System.in);
-        scanner userName = scanner.nextLine();
+        Scanner scanner = new Scanner (System.in);
+        String userName = scanner.nextLine();
 
-        System.out.println("Nice to meet you" + userName + "! What is your hobby?");
+        System.out.println("Nice to meet you " + userName + "! What is your hobby?");
 
         String userHobby = scanner.nextLine();
-        System.out.println("Well" + userName + "," + userHobby + "is a really cool hobby.");
+        System.out.println("Well " + userName + "," + userHobby + " is a really cool hobby.");
     }
 }
